7282a462d5230fa25c58d4e46c2f8cea48f59d4f
DavidABliss <davidbliss@utexas.edu>
2022-01-06 13:17:59 -0600

    Parse arguments with argparse
    Use arguments for input and output locations, instead of prompting user to type in free text

--- directory_report_generator.py
+++ directory_report_generator.py
@@ -1,46 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-This function returns the total volume in GiB of each top-level directory 
-within a user-provided path. It ignores 'loose' files located at the top level.
-
-It creates two output files in a user-provided output directory: 
-    
-The first is a spreadsheet, 'directory_report.csv'. This shows the size in GiB
-of all top-level directories in the path scanned for a given date, plus a total 
-GiB of all directories. If the report is run multiple times on the same 
-directory and the same output path is specified, new columns will be added to
-the previous spreadsheet, creating a running log of a directory's size over 
-time.
-
-The CSV output file is created using a second temporary spreadsheet, 
-directory_report_new.csv, which replaces the previous spreadsheet once each 
-directory has been scanned. This allows each version of the report to maintain
-a record of directories removed between scans, as well as new directories 
-added between scans
-
-The second output file is a human-readable text file, directory_report.txt,
-which compares each directory in the latest directory_report.csv against the 
-previous scan. This is useful for identifying which directories show the most
-growth in GiB and by percentage. The text file also notes whether a folder was
-deleted or added between scans.
-
-Although the calculation of each directory's size is in bytes, the report 
-spreadsheet rounds these to GiB. The report text file rounds large directories
-(1024 GiB or more) to TiB, but calculates absolute and percent growth in GiB. 
-This report function can be modified to leave byte counts unconverted, however
-in its original state it should not be used to determine precise changes in 
-directory size.
-
-Script written by David Bliss in February 2020
-davidallynbliss.com
-"""
-
-
-
-
 import os
 import datetime
 import csv 
+import argparse
 
 def directoryReport(rootDir, reportDir):
     # Path to three output reports (one temporary)
@@ -59,6 +20,12 @@ def directoryReport(rootDir, reportDir):
     # Create a list of directories (ignoring files) at the user-provided path
     directories = []
     topDirs = os.listdir(rootDir)
+    
+    # Remove the '.snapshot' directory from the list to skip it. This directory
+    # contains temporary archived copies of the entire directory/volume and 
+    # makes the process take several times longer if it is included
+    if '.snapshot' in topDirs:
+        topDirs.remove('.snapshot') 
     for item in topDirs:
         if os.path.isdir(rootDir + item):
             directories.append(item)
@@ -250,11 +217,17 @@ def directoryReport(rootDir, reportDir):
 
         output.write('\n ----------------------------------- \n')    
 
-# Get user input for the path to be scanned and the path to place reports.        
-user_input1 = input('Path to the drive or directory to be scanned, using forward slashes (\'/\'): ')
-user_input2 = input('Path to a directory where reports will be saved, using forward slashes (\'/\'): ')
-if not user_input1.endswith('/'):
-    user_input1 = user_input1 + '/'
-if not user_input2.endswith('/'):
-    user_input2 = user_input2 + '/'
-directoryReport(user_input1.rstrip(), user_input2.rstrip())
+# Get user input for the path to be scanned and the path to place reports.
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input', required=True, help="path to the drive or directory to be scanned", action="store", dest="i")
+parser.add_argument('-o', '--output', required=True, help="Path to a directory where reports will be saved", action="store", dest="o")
+
+args = parser.parse_args()        
+inputDir = args.i
+outputDir = args.o
+
+if not inputDir.endswith('/'):
+    inputDir = inputDir + '/'
+if not outputDir.endswith('/'):
+    outputDir = outputDir + '/'
+directoryReport(inputDir.rstrip(), outputDir.rstrip())
